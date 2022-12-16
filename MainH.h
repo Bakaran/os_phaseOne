2f22aee1fc55498c4581dc586821d8169d19c49a
SaveAsArtaz <artaz.for.git@gmail.com>
2022-12-16 14:24:33 +0330

    process class created

---
+++ MainH.h
@@ -0,0 +1,5 @@
+#ifndef MAINH_H
+#define MAINH_H
+#include <iostream>
+#include <QString>
+#endif // MAINH_H
--- main.cpp
+++ main.cpp
@@ -1,8 +1,68 @@
-#include <QCoreApplication>
+#include "MainH.h"
+class Process{
+private:
+    QString ID;
+    QString State;
+    QString File_Name;
+    int Instruction_Register;
+    int Accumulator;
+    int Temp;
+    int PC;
+public:
+    Process(QString ID,QString File_Name)
+    {
+        this->ID = ID;
+        this->File_Name = File_Name;
+        this->State = "Ready";
+        this->Instruction_Register = 0;
+        this->Accumulator = 0;
+        this->PC = 0;
+        this->Temp = 0;
+    }
+    void Set_IR(int IR_Data)
+    {
+        this->Instruction_Register = IR_Data;
+    }
+    void Set_AC(int AC_Data)
+    {
+        this->Accumulator = AC_Data;
+    }
+    void Set_Temp(int T_Data)
+    {
+        this->Temp = T_Data;
+    }
+    void Set_PC(int PC_Data)
+    {
+        this->PC = PC_Data;
+    }
+    QString Get_ID()
+    {
+        return this->ID;
+    }
+    QString Get_State()
+    {
+        return this->State;
+    }
+    int Get_IR()
+    {
+        return this->Instruction_Register;
+    }
+    int Get_AC()
+    {
+        return this->Accumulator;
+    }
+    int Get_Temp()
+    {
+        return this->Temp;
+    }
+    int Get_PC()
+    {
+        return this->PC;
+    }
 
-int main(int argc, char *argv[])
+};
+
+int main (void)
 {
-    QCoreApplication a(argc, argv);
-
-    return a.exec();
+    std::cout <<  "hello world!" <<std::endl;
 }
